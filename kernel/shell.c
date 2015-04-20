43ebab01bcd508984522067c93f7ab9a8dbceaa0
Omar Shaikh <omarasifshaikh@gmail.com>
2015-04-20 11:02:25 -0700

    Basic Shell Functionality without argument Inputs

--- kernel/shell.c
+++ kernel/shell.c
@@ -1,6 +1,7 @@
 #include <kernel.h>
-static WINDOW shell_window = {0, 10,  80, 25, 0, 0, ' '};
-char currentCommandBuffer[80];//max line length
+static WINDOW shell_window = {0, 10,  80, 15, 0, 0, ' '};
+char cmdBuffer[80];//currentCommandBuffer[max line length]
+int i=0;
 
 
 /**
@@ -16,12 +17,18 @@ char currentCommandBuffer[80];//max line length
  void printHelp(){
  	wprintf(&shell_window,"Welcome to HELP\n");
  	wprintf(&shell_window,"Following are the list of commands that you may use:\n");
- 	wprintf(&shell_window,"help  				 - for displaying help information\n");
- 	wprintf(&shell_window,"ps     				 - list all processes\n");
- 	wprintf(&shell_window,"cls    				 - clear window\n");
+ 	wprintf(&shell_window,"help                  - for displaying help information\n");
+ 	wprintf(&shell_window,"ps                    - list all processes\n");
+ 	wprintf(&shell_window,"cls                   - clear window\n");
  	wprintf(&shell_window,"echo (argument)       - clear window\n");
  	wprintf(&shell_window,"wait (argument)       - clear window\n");
  }
+/**
+ * Function that prints the processes information
+ */
+void printAllProcesses(){
+	print_all_processes(&shell_window);
+}
 
 /**
  * Function that clears the shell window
@@ -52,14 +59,52 @@ void clrBuffer(){
 	int i;
 	for ( i= 0; i < 80; ++i)
 	{
-		currentCommandBuffer[i] = 0;
+		cmdBuffer[i] = '\0';
 	}
 
 }
+/**
+ * Basic String compare Function
+ * @param1	User command 
+ * @param2	Predefined TOS command
+ * @return	Boolean
+ */
+BOOL mystrcmp(char* cmd1, char* cmd2){
+	while (*cmd1 == *cmd2 && *cmd2 != '\0') {
+        cmd1++;
+        cmd2++;
+    }
+    return *cmd2 == '\0';
+    /*
+    if (*cmd2 == '\0')
+    	return 1;
+    else
+    	return 0;
+    	*/
+}
+/**
+ * Execute the command!
+ * uses a basic string compare mechanism to find the command.
+ */
+void executeCmd(){
+	char* cmd = &cmdBuffer[0];
+	if(mystrcmp(cmd,"help")){
+		printHelp();
+	}else if(mystrcmp(cmd,"about")){
+		printAbout();
+	}else if(mystrcmp(cmd,"cls")){
+		clearShellWindow();
+	}else if(mystrcmp(cmd,"echo")){
+		echoShell();
+	}else if(mystrcmp(cmd,"wait")){
+		sleepShell();
+	}else if(mystrcmp(cmd,"ps")){
+		printAllProcesses();
+	}else{
+		wprintf(&shell_window,"Invalid command! enter help for information\n");
+	}
 
-
-
-
+}
 
 
 
@@ -88,21 +133,37 @@ void clrBuffer(){
 		output_char(&shell_window,'#');
 		
 
-		shell_window_height++;
+		//shell_window_height++;
 		while(1){
 
 			msg.key_buffer = &ch;
 			send(keyb_port, &msg);
+
 			switch(ch){
+				case '\b':
+                    if (i == 0)
+                        continue;
+                    i--;
+                    wprintf(&shell_window, "%c", ch);
+                    break;
 				case 13:
+					cmdBuffer[i]='\0';
 					wprintf(&shell_window, "\n");
+					executeCmd();
+					i=0;
+					clrBuffer();
+
+					//wprintf(&shell_window, "\n");
 					output_char(&shell_window,'#');
 					break;
 				default:
+					cmdBuffer[i] = ch;
+					i++;
 					wprintf(&shell_window, "%c", ch);
 					break;
 
 			}
+			//executeCmd();
 			/*if(ch == '\r'){
 				shell_window.cursor_x = 0;
 				shell_window.cursor_y++;
